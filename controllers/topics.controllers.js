97fb3df8392e7be479a1ff39d30576e5a82d0a57
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-05-09 15:24:20 +0100

    ticket 3.5 complete

--- controllers/topics.controllers.js
+++ controllers/topics.controllers.js
@@ -1,9 +1,20 @@
-const { fetchTopics } = require("../models/topics.models");
+const { fetchTopics, fetchApi } = require("../models/topics.models");
 
 exports.getTopics = (req, res, next) => {
   fetchTopics()
     .then((result) => {
       res.status(200).send({ topics: result });
+    })
+    .catch((err) => {
+      next(err);
+    });
+};
+
+exports.getApi = (req, res, next) => {
+  fetchApi()
+    .then((result) => {
+      const parsedResult = JSON.parse(result);
+      res.status(200).send({ result: parsedResult });
     })
     .catch((err) => {
       next(err);
