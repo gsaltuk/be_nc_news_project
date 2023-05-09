328b079824f457de69dd8830a58936147331d06c
gsaltuk <127306125+gsaltuk@users.noreply.github.com>
2023-05-09 16:50:51 +0100

    added comments to app.js

--- app.js
+++ app.js
@@ -2,16 +2,20 @@ const express = require("express");
 const { getTopics, getApi, getArticleById } = require("./controllers/topics.controllers");
 const app = express();
 
+//GET requests
+
 app.get("/api/topics", getTopics);
 app.get("/api", getApi)
 app.get('/api/articles/:article_id', getArticleById)
+
+
+//Error Handling
 
 app.use((err, req, res, next) => {
     if(err.status && err.msg){
         res.status(err.status).send({msg: err.msg})
     }
 })
-
 app.all("*", (req, res) => {
   res.status(404).send({msg: "Error - check endpoint and retry"});
 });
